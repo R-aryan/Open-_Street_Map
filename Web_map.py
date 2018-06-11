15535e98923f6d1a11ea2bf08925020aeb73ed2a
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-11 18:07:51 +0530

    code for adding marker on map

--- Web_map.py
+++ Web_map.py
@@ -2,8 +2,10 @@ import folium
 
 map= folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 
-map.add_child(folium.Marker(location=[38.2,-99.09],popup="Hi I am marker",icon=folium.Icon(color='green')))
+fg= folium.FeatureGroup(name='My map')
 
+fg.add_child(folium.Marker(location=[38.2,-99.09],popup="Hi I am marker",icon=folium.Icon(color='green')))
+map.add_child(fg)
 
 
 
