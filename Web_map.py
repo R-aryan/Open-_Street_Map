65ebb3f0925e2b516e700e426dd4df6b171deec4
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-11 20:41:27 +0530

    mapping location of all volcanoes

--- Web_map.py
+++ Web_map.py
@@ -1,12 +1,20 @@
 import folium
+import pandas as pd
 
 map= folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 
 fg= folium.FeatureGroup(name='My map')
 
-fg.add_child(folium.Marker(location=[38.2,-99.09],popup="Hi I am marker",icon=folium.Icon(color='green')))
-map.add_child(fg)
 
+data = pd.read_csv("Volcanoes.txt")
+#print(data)
+latitude= list(data['LAT'])
+longitude= list(data['LON'])
+
+for lt,ln in zip(latitude,longitude):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am marker",icon=folium.Icon(color='green')))
+
+map.add_child(fg)
 
 
 map.save("Map1.html")
