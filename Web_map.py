574b03077489d47a4ad14ca97cfdc82f422b0556
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-11 18:04:49 +0530

    aded code for creating marker

--- Web_map.py
+++ Web_map.py
@@ -1,5 +1,12 @@
 import folium
 
-map= folium.Map(location=[38.58,-99.09],zoom_start=6)
+map= folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
+
+map.add_child(folium.Marker(location=[38.2,-99.09],popup="Hi I am marker",icon=folium.Icon(color='green')))
+
+
+
+
 map.save("Map1.html")
+
 print("running Successfully")
